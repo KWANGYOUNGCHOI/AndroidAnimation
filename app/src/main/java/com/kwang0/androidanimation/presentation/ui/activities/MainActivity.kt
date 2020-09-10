71ba8fb266ef969ae47bda2d7aca85c056dc6da8
KWANGYOUNGCHOI <cky1203@gmail.com>
2020-09-10 21:23:37 +0900

    Rename about recycler class

--- app/src/main/java/com/kwang0/androidanimation/presentation/ui/activities/MainActivity.kt
+++ app/src/main/java/com/kwang0/androidanimation/presentation/ui/activities/MainActivity.kt
@@ -3,12 +3,12 @@ package com.kwang0.androidanimation.presentation.ui.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.kwang0.androidanimation.R
-import com.kwang0.androidanimation.presentation.ui.views.MainRVView
+import com.kwang0.androidanimation.presentation.ui.views.MainRecycler
 
 class MainActivity : AppCompatActivity() {
     val TAG = MainActivity::class.simpleName
 
-    private var animationView: MainRVView? = null
+    private var animationView: MainRecycler? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,7 +23,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun mainViewSetUp() {
-        animationView = MainRVView(this)
+        animationView = MainRecycler(this)
         animationView?.bindView(this)
     }
 }
